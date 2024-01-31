67c603aa06338bbffda76e8da2aa3413a426e18f
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-01-31 10:22:35 +0000

    Added Filters component

---
+++ client/src/components/FilterContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class FilterContainer extends Component
+{
+    render() {
+        return (
+            <div className = "filter-container" >
+
+            </div>
+        )
+    }
+}
