1a282563e0cc440421c8910ca5e8f1668993843a
D00253579 <D00253579@student.dkit.ie>
2024-01-31 10:21:41 +0000

    Pushing database env stuff

--- client/src/components/AddCar.js
+++ client/src/components/AddCar.js
@@ -16,10 +16,15 @@ export default class AddCar extends Component
         super(props)
 
         this.state = {
-            model:"",
+            product_id:"",
+            name:"",
             colour:"",
-            year:"",
+            size:"",
             price:"",
+            gender:"",
+            category:"",
+            brand:"",
+            current_stock:"",
             redirectToDisplayAllCars:false
         }
     }
@@ -41,14 +46,19 @@ export default class AddCar extends Component
     {
         e.preventDefault()
 
-        const carObject = {
-            model: this.state.model,
-            colour: this.state.colour,
-            year: this.state.year,
-            price: this.state.price
+        const tShirtObject = {
+            product_id:this.state.product_id,
+            name:this.state.name,
+            colour:this.state.colour,
+            size:this.state.size,
+            price:this.state.name,
+            gender:this.state.gender,
+            category:this.state.category,
+            brand:this.state.brand,
+            current_stock:this.state.current_stock,
         }
 
-        axios.post(`${SERVER_HOST}/cars`, carObject)
+        axios.post(`${SERVER_HOST}/cars`, tShirtObject)
         .then(res => 
         {   
             if(res.data)
@@ -78,26 +88,45 @@ export default class AddCar extends Component
                 {this.state.redirectToDisplayAllCars ? <Redirect to="/DisplayAllCars"/> : null}                                            
                     
                 <Form>               
-                    <Form.Group controlId="model">
-                        <Form.Label>Model</Form.Label>
-                        <Form.Control ref = {(input) => { this.inputToFocus = input }} type="text" name="model" value={this.state.model} onChange={this.handleChange} />
+                    <Form.Group controlId="product_id">
+                        <Form.Label>Product_id</Form.Label>
+                        <Form.Control ref = {(input) => { this.inputToFocus = input }} type="text" name="product_id" value={this.state.product_id} onChange={this.handleChange} />
                     </Form.Group>
-    
+
+                    <Form.Group controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                    </Form.Group>
+
                     <Form.Group controlId="colour">
                         <Form.Label>Colour</Form.Label>
                         <Form.Control type="text" name="colour" value={this.state.colour} onChange={this.handleChange} />
                     </Form.Group>
-    
-                    <Form.Group controlId="year">
-                        <Form.Label>Year</Form.Label>
-                        <Form.Control type="text" name="year" value={this.state.year} onChange={this.handleChange} />
+                    <Form.Group controlId="size">
+                        <Form.Label>Size</Form.Label>
+                        <Form.Control type="text" name="size" value={this.state.size} onChange={this.handleChange} />
                     </Form.Group>
-    
+
                     <Form.Group controlId="price">
                         <Form.Label>Price</Form.Label>
                         <Form.Control type="text" name="price" value={this.state.price} onChange={this.handleChange} />
-                    </Form.Group> 
-            
+                    </Form.Group>
+
+                    <Form.Group controlId="gender">
+                        <Form.Label>Gender</Form.Label>
+                        <Form.Control type="text" name="gender" value={this.state.gender} onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <Form.Group controlId="category">
+                        <Form.Label>Category</Form.Label>
+                        <Form.Control type="text" name="category" value={this.state.category} onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <Form.Group controlId="brand">
+                        <Form.Label>Brand</Form.Label>
+                        <Form.Control type="text" name="Brand" value={this.state.brand} onChange={this.handleChange} />
+                    </Form.Group>
+
                     <LinkInClass value="Add" className="green-button" onClick={this.handleSubmit}/>            
             
                     <Link className="red-button" to={"/DisplayAllCars"}>Cancel</Link>
