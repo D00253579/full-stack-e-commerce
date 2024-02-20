c4155e440154b3addd701f8d9b7be5caa6b4675e
D00253579 <D00253579@student.dkit.ie>
2024-02-20 00:30:41 +0000

    Created view users in the admin dashboard
    Created component for admin dashboard to allow admin to view all users.

---
+++ client/src/components/AdminDashboard/AdminUserView.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import UserTable from "./UserTable";
+
+export default class AdminUserView extends Component
+{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log("Displaying Admin view of Users")
+        return (
+            <div className="admin-table-container">
+                <UserTable users={this.props.users}
+                            handleRowClick={this.props.handleRowClick}
+                />
+            </div>
+
+        )
+    }
+
+}
