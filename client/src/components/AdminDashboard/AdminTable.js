99d88a4a90d72d3cab78731c7cbe437036d1d4aa
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-02-26 20:31:10 +0000

    Fix for name sort
    Did'nt allow for name to be sorted when the component loads.
    
    Arrow no reset to default making it easier to see which one is active

--- client/src/components/AdminDashboard/AdminTable.js
+++ client/src/components/AdminDashboard/AdminTable.js
@@ -17,23 +17,32 @@ export default class AdminTable extends Component
         let products = [...this.props.products] // soft copy of products to manipulate
 
         // sort ascending
-        if(this.state.nameIsAscending) {
+        if(!this.state.nameIsAscending) {
            let ascendingProducts = products.sort((a, b) => a.name < b.name?1:-1)
             this.props.updateProducts(ascendingProducts)
-            //console.log("Name ACE: ", ascendingProducts)
+            console.log("Name ACE: ", ascendingProducts)
         // sort descending
         } else {
             let descendingProducts = products.sort((a, b) => a.name < b.name?-1:1)
             this.props.updateProducts(descendingProducts)
-            //console.log("Name DESC: ", descendingProducts)
+            console.log("Name DESC: ", descendingProducts)
         }
 
         // When the sort is clicked depending on what the previous order was this will set it to its opposite to change the direction of the arrow
-        if(this.state.nameIsAscending) {
-            this.setState({nameIsAscending: false})
-        } else {
+        if(!this.state.nameIsAscending) {
             this.setState({nameIsAscending: true})
+        } else {
+            this.setState({nameIsAscending: false})
         }
+
+        // reset other arrows to default
+        if(!this.state.priceIsAscending || !this.state.stockIsAscending) {
+            this.setState({priceIsAscending: true})
+            this.setState({stockIsAscending: true})
+        }
+
+
+
     }
     sortPrice = () => { // default ascending
         let products = [...this.props.products]
@@ -55,6 +64,13 @@ export default class AdminTable extends Component
         } else {
             this.setState({priceIsAscending: true})
         }
+
+        // reset other arrows to default
+        if(!this.state.nameIsAscending || !this.state.stockIsAscending) {
+            this.setState({nameIsAscending: true})
+            this.setState({stockIsAscending: true})
+        }
+
     }
     sortStock = () => { // default ascending
         let products = [...this.props.products]
@@ -77,6 +93,12 @@ export default class AdminTable extends Component
         } else {
             this.setState({stockIsAscending: true})
         }
+
+        // reset other arrows to default
+        if(!this.state.nameIsAscending || !this.state.priceIsAscending) {
+            this.setState({nameIsAscending: true})
+            this.setState({priceIsAscending: true})
+        }
     }
 
 
@@ -87,11 +109,11 @@ export default class AdminTable extends Component
                 <thead>
                 <tr>
                     <th>Inventory Number</th>
-                    <th>Name <label className="sortLabel" onClick={this.sortName}><span id="priceSort">{!this.state.nameIsAscending ? '\u25BE' : '\u25B4'} </span></label></th>
+                    <th>Name <label className="sortLabel" onClick={this.sortName}><span id="priceSort">{!this.state.nameIsAscending ? '\u25B4' : '\u25BE'} </span></label></th>
                     <th>Brand</th>
                     <th>Category</th>
-                    <th>Price <label className="sortLabel" onClick={this.sortPrice}><span id="priceSort">{this.state.priceIsAscending ? '\u25B4' : '\u25BE'}</span></label></th>
-                    <th>Stock <label className="sortLabel" onClick={this.sortStock}><span id="stockSort">{this.state.stockIsAscending ? '\u25B4' : '\u25BE'}</span></label></th>
+                    <th>Price <label className="sortLabel" onClick={this.sortPrice}><span id="priceSort">{!this.state.priceIsAscending ? '\u25B4' : '\u25BE'}</span></label></th>
+                    <th>Stock <label className="sortLabel" onClick={this.sortStock}><span id="stockSort">{!this.state.stockIsAscending ? '\u25B4' : '\u25BE'}</span></label></th>
                     <th>P. Code</th>
                 </tr>
                 </thead>
