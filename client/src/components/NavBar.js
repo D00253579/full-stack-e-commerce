b14344de3daac078fa2f7ff1ee12d222fb264744
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-02-26 23:12:18 +0000

    Basic Search Functionality
    Can now search by name or brand. A dropdown menu appears below the search bar display the results, when clicked you are brought to EditProduct

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,26 +7,93 @@ import SearchIcon from "../Images/SearchIcon.png"
 import login from "./Login/Login";
 import {SERVER_HOST} from "../config/global_constants";
 import ShoppingCart from "./ShoppingCart";
+import SearchDropdown from "./SearchDropdown";
+import axios from "axios";
 
 export default class NavBar extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+            searchResults: [],
+            searchInput: "",
+        }
+    }
+    componentDidMount() {
+        // Fetch products in the parent component
+        axios.get(`${SERVER_HOST}/products`)
+            .then((res) => {
+                if (res.data) {
+                    if (res.data.errorMessage) {
+
+                        console.log(res.data.errorMessage);
+                    } else {
+                        console.log("Records read to Admin dashboard");
+                        this.setState({
+                            products: res.data,
+                        });
+                    }
+                } else {
+                    console.log("Record not found");
+                }
+            });
+    }
+
+    handleSearchChange = (e) => {
+        this.setState({searchInput: e.target.value})
+        if(this.state.searchInput.length > 0) {
+            this.handleProductSearch()
+        } else {
+            this.setState({searchResults: []})
+        }
+    }
+
+    handleProductSearch = () => {
+        let matchingProducts = this.state.products.filter((product) =>
+            (product.name.toLowerCase().includes(this.state.searchInput.toLowerCase()) ||
+            product.brand.toLowerCase().includes(this.state.searchInput.toLowerCase()))
+        )
+
+        matchingProducts.sort((a, b) => a.name < b.name?-1:1)
+        this.setState({searchResults: matchingProducts})
+        console.log("Found Products: ",matchingProducts)
+    }
 
     render() {
+        console.log("Search results in NavBar: ",this.state.searchResults)
         return (
             <div className="nav-container">
                 <nav>
                     <div className={"container"}>
                         <div className={"search-container"}>
                             <div className="search-bar">
-                                <input placeholder="Search" type="text" id="search"/>
+                                <input
+                                    placeholder="Search"
+                                    type="text"
+                                    id="search"
+                                    onChange={this.handleSearchChange}
+                                />
+                                {this.state.searchInput.length > 0 ?
+                                    <SearchDropdown
+                                        searchResults={this.state.searchResults}
+                                    />
+                                    : null
+                                }
                             </div>
                             <div className={"icons-container"}>
-                                <i className={"searchIcon"}><img src={SearchIcon}/></i>
+                                <i className={"searchIcon"} onClick={this.handleProductSearch}>
+                                    <img src={SearchIcon}/>
+                                </i>
                             </div>
                         </div>
+
                         <div className={"icons-container"}>
-                            <i className={"logo-image"}><img src={Logo} alt="logo"/></i>
+                            <i className={"logo-image"}>
+                                <img src={Logo} alt="logo"/>
+                            </i>
                         </div>
+
                         <div className={"icons-container"}>
                             <Link to={"/AccountPage"}>
                                 <i className={"account"}>
