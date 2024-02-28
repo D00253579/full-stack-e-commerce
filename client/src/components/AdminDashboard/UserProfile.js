a236d6968c52659bf155fa9d6be21a14d24b9dd8
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-02-28 21:37:56 +0000

    saving before merge

--- client/src/components/AdminDashboard/UserProfile.js
+++ client/src/components/AdminDashboard/UserProfile.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import NavBar from "../NavBar";
 import axios from "axios";
 import {SERVER_HOST} from "../../config/global_constants";
+import {Redirect} from "react-router-dom";
 
 export default class UserProfile extends Component {
 
@@ -41,22 +42,29 @@ export default class UserProfile extends Component {
 
     render() {
         return (
-            <div className="user-profile">
+            <div>
+                {this.state.redirectToDashboard ? <Redirect to={"/AdminDashboard/ViewUsers/"}/> : null}
                 <div className="admin-head-container">
                     <NavBar/>
                 </div>
 
                 <h1>User Profile</h1>
-                <div className="profile-container">
+                <div  className="user-profile">
 
-                    <div className="profile-left">
-                        <h4>Name</h4>
-                    </div>
-                    <div className="profile-right">
-                        <h4>Name</h4>
-                    </div>
+                    <div className="profile-container">
+
+                        <div className="profile-left">
+                            {/*TODO this is where the user profile will go*/}
+
+                        </div>
+                        <div className="profile-right">
+                            <h2>Name: <span className="profile-span">{this.state.user.name}</span></h2>
+                            <h2>Email: <span className="profile-span">{this.state.user.email}</span></h2>
+                        </div>
 
                 </div>
+                </div>
+                <button onClick={this.handleReturn}>Return</button>
             </div>
         )
     }
