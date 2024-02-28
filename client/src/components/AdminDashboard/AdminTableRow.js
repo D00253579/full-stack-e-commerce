e9fc83df2a25427781c21dca781e0673b91e3362
D00253579 <D00253579@student.dkit.ie>
2024-02-28 17:03:20 +0000

    Adding the ability to add a photo to products and displaying products on MainPage
    Allowing the admin to add multiple photos when creating a product, displaying photos on the main page and fixing up the tshirt json to match database.

--- client/src/components/AdminDashboard/AdminTableRow.js
+++ client/src/components/AdminDashboard/AdminTableRow.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import {Redirect} from "react-router-dom";
-
+import {SERVER_HOST} from "../../config/global_constants";
+import axios from "axios";
 export default class AdminTableRow extends Component
 {
     constructor(props) {
@@ -9,6 +10,22 @@ export default class AdminTableRow extends Component
             rowIsClicked: false
         }
     }
+componentDidMount() {
+        this.props.product.photos.map(photo =>{
+            return axios.get(`${SERVER_HOST}/products/photo/${photo.filename}`)
+                .then(res =>{
+                    if (res.data){
+                        if (res.data.errorMessage){
+                            console.log(res.data.errorMessage)
+                        }else{
+                            document.getElementById(photo._id).src=`data:;base64,${res.data.image}`
+                        }
+                    }else{
+                        console.log("Record not found")
+                    }
+                })
+        })
+}
 
     handleRowClick = () => {
         this.setState({rowIsClicked: true})
@@ -22,6 +39,7 @@ export default class AdminTableRow extends Component
             category,
             brand,
             current_stock,
+            photos
         } = this.props.product;
 
         return (
@@ -48,6 +66,7 @@ export default class AdminTableRow extends Component
                     <td>{price}</td>
                     <td>{current_stock}</td>
                     <td>{product_id}</td>
+                    <td className="photos">{photos.map(photo =><img key={photo._id} id={photo._id} alt=""/>)}</td>
                 </tr>
             )
         )
