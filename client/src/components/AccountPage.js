7794dc6181d030a38e2e77ede17b2fa1a14e1a68
D00253579 <146189574+D00253579@users.noreply.github.com>
2024-02-29 19:03:46 +0000

    Revert "Register page styling and layout is complete."

--- client/src/components/AccountPage.js
+++ client/src/components/AccountPage.js
@@ -22,16 +22,16 @@ export default class AccountPage extends Component {
                 <div className="account-head-container">
                     <NavBar/>
                 </div>
-           {localStorage.accessLevel > ACCESS_LEVEL_GUEST ?
-                <div className="account-container">
-                    <div className="account-box">
-                        <div className="login-box">
-                            <h1>WELCOME BACK!</h1>
-                            <label> Email Address:<span> *</span> </label>
-                            <input type="text" id="email-input"/><br/><br/>
-                            <label>Password:<span> *</span></label>
-                            <input type="password" id="password-input"/>
-                            <div className="register-box">
+                {localStorage.accessLevel > ACCESS_LEVEL_GUEST ?
+                    <div className="account-container">
+                        <div className="account-box">
+                            <div className="login-box">
+                                <h1>WELCOME BACK!</h1>
+                                <h2> Email Address: </h2>
+                                <input type="text" id="address"/>
+                                <h2>Password:</h2>
+                                <input type="text" id="postcode"/>
+                                <div className="register-box">
                                     <Link to={"/Login/login"}>
                                         <button className={"signIn-btn"}>SIGN IN</button>
                                     </Link>
